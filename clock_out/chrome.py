93e8ba38fe63f015f0d0b6a0a8c4fc9c109e84cd
Alvin <alvinlin425@gmail.com>
2023-11-12 19:05:33 -0500

    Run on weekdays only & relog when need to punch

--- clock_out/chrome.py
+++ clock_out/chrome.py
@@ -31,25 +31,32 @@ class gc_driver():
     cookie_path = 'pcm_cookies.csv'
 
     def __init__(self):
+        # start_time = time.time()
         load_dotenv()
         self.driver_path = Service(os.environ['GC_DRIVER_PATH'])
-        self.ff_options = webdriver.ChromeOptions()
-        # self.ff_options.add_argument('--headless')
-        self.ff_options.add_argument("--window-size=1920,1080")
-        self.ff_options.add_argument("--disable-extensions")
-        self.ff_options.add_argument("--proxy-server='direct://'")
-        self.ff_options.add_argument("--proxy-bypass-list=*")
-        self.ff_options.add_argument("--disable-extensions")
-        self.ff_options.add_argument("--disable-gpu")
+        self.gc_options = webdriver.ChromeOptions()
+        self.gc_options.add_argument("--log-level=2")
+        self.gc_options.add_argument('--headless')
+        self.gc_options.add_argument("--window-size=1920,1080")
+        self.gc_options.add_argument("--disable-extensions")
+        self.gc_options.add_argument("--proxy-server='direct://'")
+        self.gc_options.add_argument("--proxy-bypass-list=*")
+        self.gc_options.add_argument("--disable-extensions")
+        self.gc_options.add_argument("--disable-gpu")
         self.driver = webdriver.Chrome(
-            service=self.driver_path, options=self.ff_options)
+            service=self.driver_path, options=self.gc_options)
+        # end_time = time.time()
+        # print(f'Driver bootup took: {end_time-start_time}')  # took about 2 seconds to load
 
-    def boot_gc(self):
+    def run_pcm(self):
+        # start_time = time.time()
         pay_com_login_url = 'https://www.paycomonline.net/v4/ee/web.php/app/login'
         self.driver.get(pay_com_login_url)
         self.login()  # can fail if cookies expired or credentials are wrong
         self.navi2_clock_page()  # can fail if website updates and xpath is changed
-        self.start_scheduler()
+        # end_time = time.time() # took about 6 seconds to log in and navigate
+        # print(f'Loggin on and navigating took: {end_time-start_time}')
+        # self.start_scheduler()
         # print(self.driver.page_source)
         # self.driver.close()
 
@@ -70,7 +77,8 @@ class gc_driver():
             submit_btn.click()
             print('Successfully logged in')
         except:
-            logging.error('Login Unsuccessful: Check credentials + 2fa cookie')
+            logging.error(
+                'Login Unsuccessful: Check credentials + 2fa cookie')
             self.driver.close()
 
     def navi2_clock_page(self):
@@ -89,6 +97,7 @@ class gc_driver():
         # logging.warning(self.driver.page_source)      # caution of auto logging out
         # print(f'Attemping 2 punch {xpath}')
         try:
+            self.run_pcm()  # relog [ About 8 seconds on a good day ]
             punch_in_btn = WebDriverWait(self.driver, 5).until(
                 EC.presence_of_element_located((By.XPATH, xpath)))
             punch_in_btn.click()
@@ -96,6 +105,7 @@ class gc_driver():
         except:
             logging.error(
                 f'Punch In Was Unsuccessful... Check XPATH: {xpath} ')
+            # reboot if cant punch
         # print(self.driver.page_source)
 
     def reset_page_timer(self):
@@ -107,11 +117,55 @@ class gc_driver():
         # print(soup.prettify)
 
     def start_scheduler(self):
-        schedule.every(25).minutes.do(self.reset_page_timer)
-        schedule.every().day.at("09:00").do(lambda: self.punch_in(in_day))  # 9 AM EST
-        schedule.every().day.at("12:00").do(lambda: self.punch_in(out_lunch))  # 12 PM EST
-        schedule.every().day.at("13:00").do(lambda: self.punch_in(in_lunch))  # 1 PM EST
-        schedule.every().day.at("17:59").do(lambda: self.punch_in(out_day))  # 5:59 PM EST
+        # schedule.every(25).minutes.do(self.reset_page_timer)       # instead of keeping the site live just relog???
+        # ---------------------------------------------------------------------------------------
+        schedule.every().monday.at("09:00").do(
+            lambda: self.punch_in(in_day))  # 9 AM EST
+        schedule.every().monday.at("12:00").do(
+            lambda: self.punch_in(out_lunch))  # 12 PM EST
+        schedule.every().monday.at("13:00").do(
+            lambda: self.punch_in(in_lunch))  # 1 PM EST
+        schedule.every().monday.at("17:59").do(
+            lambda: self.punch_in(out_day))  # 5:59 PM EST
+        # ---------------------------------------------------------------------------------------
+        schedule.every().tuesday.at("09:00").do(
+            lambda: self.punch_in(in_day))  # 9 AM EST
+        schedule.every().tuesday.at("12:00").do(
+            lambda: self.punch_in(out_lunch))  # 12 PM EST
+        schedule.every().tuesday.at("13:00").do(
+            lambda: self.punch_in(in_lunch))  # 1 PM EST
+        schedule.every().tuesday.at("17:59").do(
+            lambda: self.punch_in(out_day))  # 5:59 PM EST
+        # ---------------------------------------------------------------------------------------
+        schedule.every().wednesday.at("09:00").do(
+            lambda: self.punch_in(in_day))  # 9 AM EST
+        schedule.every().wednesday.at("12:00").do(
+            lambda: self.punch_in(out_lunch))  # 12 PM EST
+        schedule.every().wednesday.at("13:00").do(
+            lambda: self.punch_in(in_lunch))  # 1 PM EST
+        schedule.every().wednesday.at("17:59").do(
+            lambda: self.punch_in(out_day))  # 5:59 PM EST
+        # ---------------------------------------------------------------------------------------
+        schedule.every().thursday.at("09:00").do(
+            lambda: self.punch_in(in_day))  # 9 AM EST
+        schedule.every().thursday.at("12:00").do(
+            lambda: self.punch_in(out_lunch))  # 12 PM EST
+        schedule.every().thursday.at("13:00").do(
+            lambda: self.punch_in(in_lunch))  # 1 PM EST
+        schedule.every().thursday.at("17:59").do(
+            lambda: self.punch_in(out_day))  # 5:59 PM EST
+        # ---------------------------------------------------------------------------------------
+        schedule.every().friday.at("09:00").do(
+            lambda: self.punch_in(in_day))  # 9 AM EST
+        schedule.every().friday.at("12:00").do(
+            lambda: self.punch_in(out_lunch))  # 12 PM EST
+        schedule.every().friday.at("13:00").do(
+            lambda: self.punch_in(in_lunch))  # 1 PM EST
+        schedule.every().friday.at("17:59").do(
+            lambda: self.punch_in(out_day))  # 5:59 PM EST
+        # ---------------------------------------------------------------------------------------
+        # schedule.every().sunday.at("19:01").do(lambda: self.run_pcm())
+        # schedule.every().sunday.at("19:02").do(lambda: self.run_pcm())
         print('Scheduled ...')
         while True:
             schedule.run_pending()
@@ -131,8 +185,9 @@ class gc_driver():
                 self.driver.add_cookie(key)
             except:
                 logging.error(f'Adding Cookie Fail: {key} ')
+                break
         self.driver.refresh()
 
 
 script = gc_driver()
-script.boot_gc()
+script.start_scheduler()
