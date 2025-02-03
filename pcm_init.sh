a3906f77a3b9335b0c18f7756358aadd1678850b
Alvin <Alvinlin425@gmail.com>
2025-02-03 15:01:00 -0500

    mac exec file format

---
+++ pcm_init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the directory
+cd "/Users/L/Documents/Codelioco/pcmpuncher/"
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Run your Python script
+python3 clock_out/chrome.py
